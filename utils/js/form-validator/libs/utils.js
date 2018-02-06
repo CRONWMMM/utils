e9324a2793999ecc79ff16a596e10945835a59f9
CRONWMMM <kimmmiy@sina.cn>
2018-02-06 21:38:34 +0800

    添加typeOf方法语法糖

---
+++ utils/js/form-validator/libs/utils.js
@@ -0,0 +1,38 @@
+
+
+'use strict'
+
+
+// 类型检测
+function typeOf(obj){
+	const toString = Object.prototype.toString
+	const map = {
+		'[object Boolean]'	 : 'boolean',
+		'[object Number]' 	 : 'number',
+		'[object String]' 	 : 'string',
+		'[object Function]'  : 'function',
+		'[object Array]' 	 : 'array',
+		'[object Date]' 	 : 'date',
+		'[object RegExp]'	 : 'regExp',
+		'[object Undefined]' : 'undefined',
+		'[object Null]' 	 : 'null',
+		'[object Object]' 	 : 'object'
+	}
+	return map[toString.call(obj)]
+}
+function isNumber(obj) {
+	return typeOf(obj) === 'number' ? true : false
+}
+function isString(obj) {
+	return typeOf(obf) === 'string' ? true : false
+}
+function isFunction(obj) {
+	return typeOf(obj) === 'function' ? true : false
+}
+function isArray(obj) {
+	return typeOf(obj) === 'array' ? true : false
+}
+function isObject(obj) {
+	return typeOf(obj) === 'object' ? true : false
+}
+
--- utils/js/utils.js
+++ utils/js/utils.js
@@ -63,6 +63,21 @@
 		};
 		return map[toString.call(obj)];
 	}
+	function isNumber(obj) {
+		return typeOf(obj) === 'number' ? true : false
+	}
+	function isString(obj) {
+		return typeOf(obf) === 'string' ? true : false
+	}
+	function isFunction(obj) {
+		return typeOf(obj) === 'function' ? true : false
+	}
+	function isArray(obj) {
+		return typeOf(obj) === 'array' ? true : false
+	}
+	function isObject(obj) {
+		return typeOf(obj) === 'object' ? true : false
+	}
 
 
 
